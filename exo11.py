993da40a82cf61125ce3ed23c5ec334045106c60
bezriche <ramybezriche@gmail.com>
2024-12-11 20:24:56 +0100

    exo 11 and exo 12

---
+++ exo11.py
@@ -0,0 +1,3 @@
+width=int(input("please enter the width of hash characters: "))
+
+print("#"*width)
---
+++ exo12.py
@@ -0,0 +1,5 @@
+width=int(input("please enter the width of hash characters: "))
+height=int(input("please enter the height of hash characters: "))
+
+for i in range(height):
+    print("#"*width)
