1f2810c8d345a154586e6df94e9d866d2ce07169
bezriche ramy <160641434+bezriche-ramy@users.noreply.github.com>
2025-01-15 10:39:37 +0100

    Create exo24.py

---
+++ exo24.py
@@ -0,0 +1,19 @@
+def dict_char(str):
+    char_counter = {}
+    for char in str:
+        if char in char_counter:
+            char_counter[char] +=1
+        else:
+            char_counter[char]=1
+    return char_counter
+
+
+def anagrams(str1 :str,str2:str):
+    if len(str1)!=len(str2):
+        return False
+    return dict_char(str1) == dict_char(str2)
+
+
+print(anagrams("abc","der"))
+print(anagrams("hii","hello"))
+print(anagrams("nacb","bbb"))
